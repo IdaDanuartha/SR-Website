38af7177840759dc50386898de43b542e34e4a25
IdaDanuartha <idadanuartha42@smk.belajar.id>
2021-09-01 15:24:55 +0800

    membuat form contact bisa digunakan dan memperbaiki yang lainnya

---
+++ bootstrap-4.6.0-dist/js/script.js
@@ -0,0 +1,24 @@
+// untuk menghubungkan form contact dengan google spreadsheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzTQOhLFSrefwecvYnY9v8cWd7_YuslwmBJ7CmwbH9OIcZtWpuQh9oyB1tgaq58-u274A/exec'
+const form = document.forms['submit-to-google-sheet']
+const loading = document.querySelector('.spinner-loading');
+const btnSubmit = document.querySelector('.contact-kirim');
+const alert = document.querySelector('.alert-contact');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    btnSubmit.classList.toggle('d-none');
+    loading.classList.toggle('d-none');
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+            btnSubmit.classList.toggle('d-none');
+            loading.classList.toggle('d-none');
+            alert.classList.toggle('d-none');
+            form.reset();
+
+            console.log('Success!', response)
+        })
+
+      .catch(error => console.error('Error!', error.message))
+  })
